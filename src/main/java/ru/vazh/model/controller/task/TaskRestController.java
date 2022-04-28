31d8e9409c2365db3af70643fb7bec29494512bb
Valerii <valeriiowlzharkov@gmail.com>
2022-04-29 00:47:36 +0300

    add controller classes

---
+++ src/main/java/ru/vazh/model/controller/task/TaskRestController.java
@@ -0,0 +1,7 @@
+package ru.vazh.model.controller.task;
+
+import ru.vazh.service.TaskService;
+
+public class TaskRestController {
+    private TaskService service;
+}
---
+++ src/main/java/ru/vazh/model/controller/user/AbstractUserController.java
@@ -0,0 +1,4 @@
+package ru.vazh.model.controller.user;
+
+public class AbstractUserController {
+}
